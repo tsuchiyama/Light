74d54fd28b1be6737f4aa71446300a9beb99e5ec
Gary Tsuchiyama <gtsuchiyama@student.ccc.edu>
2018-04-30 09:16:07 -0500

    First upload of Light project

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var lightOn = true
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    func updateUI(){
+        view.backgroundColor = lightOn ? .green : .red
+        }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
